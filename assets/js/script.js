1c1ed0453247cbdd810df40ccd6d85cfec1850bb
dooco <padraigdooley@gmail.com>
2022-08-12 10:27:51 +0000

    add main section to html

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+console.log("link is working");
+/**
+ * Declare constants for DOM elements and choices
+ * 
+ */
+ const buttons = document.getElementsByClassName('control');
+ const playerScore = document.getElementById('player-score');
+ const computerScore = document.getElementById('computer-score');
+ const playerImage = document.getElementById('player-image');
+ const computerImage = document.getElementById('computer-image');
+ const messages =document.getElementById('messages');
+ const choices = ["rock","paper","scissors","lixard","spock"];
+ /**
+  * Add event listener to buttons
+  */
+ for (let button of buttons) {
+    button.addEventListener('click', function() {
+        let playerChoice = this.getAttribute('data-choice');
+        playGame(playerChoice);
+    });
+ }
+ /**
+  * Main game function - accepts one paramater: data-choice value of selected button
+  */
+ function playGame(playerChoice) {
+    
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+let computerChoice = Math.floor(Math.random() * 5);
+
+computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+computerImage.alt = choices[computerChoice];
+let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+updateScores(result);
+ }
+ 
