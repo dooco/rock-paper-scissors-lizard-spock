d5fbf92edb9b4fceeb5bc9801edfab4266517f6e
dooco <padraigdooley@gmail.com>
2022-08-12 07:29:21 +0000

    initial setup html,cs, js

---
+++ assets/js/scrip.js
@@ -0,0 +1,18 @@
+/**
+ * Declare constants for DOM elements and choices
+ * 
+ */
+ const buttons = document.getElementsByClassName('control');
+ const playerScore = document.getElementById('player-score');
+ const computerScore = document.getElementById('computer-score');
+ const playerImage = document.getElementById('player-image');
+ const computerImage = document.getElementById('computer-image');
+ const messages =document.getElementById('messages');
+ const choices = ["rock","paper","scissors","lixard","spock"];
+ /**
+  * Add event listener to buttons
+  */
+ for (let button of buttons) {}
+ 
+ 
+ 
